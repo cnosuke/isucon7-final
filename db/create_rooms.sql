5888e43fc6491873cdb4859c4c1db5e5499b6fdf
Shinnosuke Takeda <shinnosuke@gmail.com>
2017-11-25 13:48:43 +0900

    Fix db script and sysytemd config on leader node

--- db/create_rooms.sql
+++ db/create_rooms.sql
@@ -4,6 +4,7 @@ CREATE TABLE `rooms` (
   PRIMARY KEY (`room_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+INSERT INTO rooms VALUES('dummy0', 0);
 INSERT INTO rooms VALUES('dummy1', 1);
 INSERT INTO rooms VALUES('dummy2', 2);
 INSERT INTO rooms VALUES('dummy3', 3);
