2ced1c4f1332203dfa1b65114bc2fcdb38702141
Shinnosuke Takeda <shinnosuke@gmail.com>
2017-11-25 14:39:11 +0900

    Use golang

--- webapp/ruby/game.rb
+++ webapp/ruby/game.rb
@@ -5,7 +5,7 @@ require 'mysql2'
 
 class Game
   HOSTS = %w(
-    app0131.isu7f.k0y.org
+    app0131.isu7f.k0y.org:8080
     app0132.isu7f.k0y.org
     app0133.isu7f.k0y.org
     app0134.isu7f.k0y.org
