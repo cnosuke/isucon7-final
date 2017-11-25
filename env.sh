1b6fe274d4b7a300eb3e2d84a5196c73778eb533
app2 <isucon@example.com>
2017-11-25 12:17:47 +0900

    Add env.sh

---
+++ env.sh
@@ -0,0 +1,6 @@
+#ISU_DB_HOST=app0134
+ISU_DB_HOST=localhost
+ISU_DB_USER=isucon
+ISU_DB_PASSWORD=isucon
+ISU_WEB_HOSTS=app0131.isu7f.k0y.org,app0132.isu7f.k0y.org,app0133.isu7f.k0y.org,app0134.isu7f.k0y.org
+
